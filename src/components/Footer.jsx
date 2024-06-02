b96624ebbce20bf5d1d6cff4e00c8da4c64d7b8a
Najas06 <najasneju@gmail.com>
2024-06-02 12:23:01 +0530

    error1 not found

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -19,21 +19,21 @@ const Footer = () => {
                 <div className='px-6 flex max-sm:flex-col'>
                     <div className='w-1/4 flex-col flex text-white gap-2 max-sm:mb-5'>
                         <div className='mt-6 max-sm:mt-0'></div>
-                        <a href="">Contact us</a>
-                        <a href="">Email:chefai@gmail.com</a>
-                        <a href="">Ph: +123 456 789</a>
+                        <a >Contact us</a>
+                        <a >Email:chefai@gmail.com</a>
+                        <a >Ph: +123 456 789</a>
                     </div>
                     <div className='w-1/4 text-white flex flex-col gap-2 max-sm:mb-5'>
                         <h3 className='font-bold underline'>Quick Links</h3>
-                        <a href="">Home</a>
-                        <a href="">Post</a>
-                        <a href="">About us</a>
+                        <a >Home</a>
+                        <a >Post</a>
+                        <a >About us</a>
                     </div>
                     <div className='w-1/4 text-white flex flex-col gap-2 items-center max-sm:items-start max-sm:mb-5'>
                         <h3 className='font-bold underline'>Follow us</h3>
-                        <a href=""><FaInstagram className='text-2xl' /></a>
-                        <a href=""><FaFacebookSquare className='text-2xl' /></a>
-                        <a href=""><FaXTwitter className='text-2xl' /></a>
+                        <a ><FaInstagram className='text-2xl' /></a>
+                        <a ><FaFacebookSquare className='text-2xl' /></a>
+                        <a ><FaXTwitter className='text-2xl' /></a>
                     </div>
                     <div className='w-1/4 text-white flex flex-col gap-2 items-center max-sm:w-full'>
                         <h3 className='font-bold underline'>Subscribe us</h3>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,8 +20,8 @@ const Header = () => {
                 </Link>
                 <div className='flex gap-6 items-center max-sm:hidden mt-3'>
                     <Link to={'/home'}><a className='transition duration-150 text-[#2A373E] font-semibold hover:text-[#FF725E] hover:ease-out hover:duration-300'>Home</a></Link>
-                    <Link to={'/posts'}><a href="" className='transition duration-150 text-[#2A373E] font-semibold hover:text-[#FF725E] hover:ease-out hover:duration-300'>Post</a></Link>
-                    <a href="" className='transition duration-150 text-[#2A373E] font-semibold hover:text-[#FF725E] hover:ease-out hover:duration-300'>About us</a>
+                    <Link to={'/posts'}><a  className='transition duration-150 text-[#2A373E] font-semibold hover:text-[#FF725E] hover:ease-out hover:duration-300'>Post</a></Link>
+                    <a  className='transition duration-150 text-[#2A373E] font-semibold hover:text-[#FF725E] hover:ease-out hover:duration-300'>About us</a>
                     <button className='transition duration-150 bg-[#FF725E] text-white px-4 py-1 rounded-xl font-semibold hover:text-[#2A373E]'>Sign in</button>
                     <button className='transition duration-150 bg-[#263238] text-white px-4 py-1 rounded-xl font-semibold hover:text-[#FF725E]'>Log out</button>
                 </div>
@@ -44,7 +44,7 @@ const Header = () => {
                                         delay: 0.7,
                                         ease: [0, 0.71, 0.2, 1.01]
                                     }}
-                                    href="" className='transition duration-150 text-xl text-[#2A373E] font-semibold hover:text-[#FF725E] hover:ease-out hover:duration-300'>Home</motion.a>
+                                     className='transition duration-150 text-xl text-[#2A373E] font-semibold hover:text-[#FF725E] hover:ease-out hover:duration-300'>Home</motion.a>
                                 <motion.a
                                     initial={{ opacity: 0 }}
                                     animate={{ opacity: 1 }}
@@ -53,7 +53,7 @@ const Header = () => {
                                         delay: 0.8,
                                         ease: [0, 0.71, 0.2, 1.01]
                                     }}
-                                    href="" className='transition duration-150 text-xl text-[#2A373E] font-semibold hover:text-[#FF725E] hover:ease-out hover:duration-300'>Post</motion.a>
+                                     className='transition duration-150 text-xl text-[#2A373E] font-semibold hover:text-[#FF725E] hover:ease-out hover:duration-300'>Post</motion.a>
                                 <motion.a
                                     initial={{ opacity: 0 }}
                                     animate={{ opacity: 1 }}
@@ -62,7 +62,7 @@ const Header = () => {
                                         delay: 0.9,
                                         ease: [0, 0.71, 0.2, 1.01]
                                     }}
-                                    href="" className='transition duration-150 text-xl text-[#2A373E] font-semibold hover:text-[#FF725E] hover:ease-out hover:duration-300'>About us</motion.a>
+                                     className='transition duration-150 text-xl text-[#2A373E] font-semibold hover:text-[#FF725E] hover:ease-out hover:duration-300'>About us</motion.a>
 
                                 <motion.button
                                     initial={{ opacity: 0 }}
